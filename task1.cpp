5bd3699349d14fa8b54d2e9280bf9acaa2f23429
Abdullah Khan <abdullah@example.com>
2025-10-27 01:47:50 -0700

    Added task1.cpp for Lab Assignment 1

---
+++ task1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Abdullah" << endl;
+    cout << "Registration Number: 12345" << endl;
+    cout << "Course Name: Software Construction and Development CA" << endl;
+    return 0;
+}
